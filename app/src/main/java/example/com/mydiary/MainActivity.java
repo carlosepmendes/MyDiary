866b42b6dfabe932e8fec325a4801a41b28dd56f
carlosepmendes <carlosepmendes@gmail.com>
2015-08-03 15:36:17 +0000

    Added logic to write and read to files

--- app/src/main/java/example/com/mydiary/MainActivity.java
+++ app/src/main/java/example/com/mydiary/MainActivity.java
@@ -1,38 +1,98 @@
 package example.com.mydiary;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private EditText enterText;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        enterText = (EditText) findViewById(R.id.editText);
+        saveButton = (Button) findViewById(R.id.saveButton);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (enterText.getText().toString().equals("")){
+                    String data = enterText.getText().toString();
+                    writeToFile(data);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Please enter text!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (readFromFile() != null) {
+            enterText.setText(readFromFile());
+        }else{
+
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    private void writeToFile(String myData){
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter( openFileOutput("diary.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(myData);
+            outputStreamWriter.close(); //always close streams
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFromFile(){
+        String result = "";
+
+        try{
+            InputStream inputStream = openFileInput("diary.txt");
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String tempString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((tempString = bufferedReader.readLine())!=null){
+                    stringBuilder.append(tempString);
+                }
+
+                inputStream.close();
+
+                result = stringBuilder.toString();
+            }
+
+        }catch (FileNotFoundException e){
+            Log.v("MyActivity", "File not found" + e.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return result;
+    }
+
 }
